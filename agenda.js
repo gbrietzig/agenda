af84247b31eb506c08b2aac8e425581116be148c
gbrietzig <57928418+gbrietzig@users.noreply.github.com>
2021-12-20 06:12:21 -0300

    Primeira reunião

--- agenda.js
+++ agenda.js
@@ -1,14 +1,13 @@
-// classe contato
+// classe
 class Contato {
-    constructor(id, name, email){
+    constructor(id, nome, email){
         this.id=id
-        this.name=name
+        this.nome=nome
         this.email=email
     }
 }
 
-// salvar no navegador
-const STORAGE = {
+const funcStorage = {
     //buscar
     get(){
         return JSON.parse(localStorage.getItem("agenda:contatos")) || []
@@ -19,67 +18,107 @@ const STORAGE = {
     }
 }
 
-const CONTATO = {
-    contatos: STORAGE.get(),
-    criarContato(){
-        const contato = new Contato(UTILS.createId(this.contatos), FORMS.name.value, FORMS.email.value)
-        CONTATO.contatos.push(contato)
-        STORAGE.set(CONTATO.contatos)
-    }
-}
+const funcContato = {
+    contatos: funcStorage.get(),
 
-const UTILS = {
-    createId(contatos) {
-        lengthContatos=contatos.length
-        if (lengthContatos == 0){
+    adicionarContato() {       
+        const id=this.criarId()
+        const nome= funcHTML.nome.value
+        const email= funcHTML.email.value
+    
+        const novoContato= new Contato(id, nome, email)
+    
+        this.contatos.push(novoContato)
+        funcStorage.set(this.contatos)
+
+        //reestrura a página
+        iniciar()
+    },
+    
+    editarContato() {
+    
+    },
+    
+    excluirContato(id) {
+        // id = id do elemento clicado
+
+        // busca todo o elemento na lista pelo id
+        const contatoSelecionado=this.obterContato(id)
+
+        // pega a posição do elemento, buscando o elemento na lista
+        index=this.contatos.indexOf(contatoSelecionado)
+        console.log(index)
+        // na posição index, elimina apenas 1 elemento
+        this.contatos.splice(index, 1)
+
+        console.log(this.contatos)
+
+        // atualiza a lista no storage
+        funcStorage.set(this.contatos)
+
+        //reestrura a página
+        iniciar()
+    
+    },
+
+    criarId() {
+        const lengthContatos = this.contatos.length
+        if (lengthContatos==0){
             return 1
         }
-        else {
-            lastContact=contatos[lengthContatos-1]
-            return lastContact.id+1
+        else{
+
+            index=lengthContatos-1
+            ultimoContato=this.contatos[index]
+            idUltimoContato=ultimoContato.id
+            novoId=idUltimoContato+1
+
+            return novoId
         }
-    }    
-}
-
-const FORMS = {
-    name: document.querySelector('input#name'),
-    email: document.querySelector('input#email')
-}
-
-const TABLE = {
-    tbody: document.querySelector('tbody'),
-
-    clearTable(){
-        TABLE.tbody.innerHTML=``
     },
-
-    navigateContacts(){
-        const contatos = CONTATO.contatos
-        contatos.forEach(contato => {
-            TABLE.addContact(contato)            
-        });
-    },
-
-    addContact(contato){
-        const tr = document.createElement('tr')
-        tr.innerHTML=`
-            <td>${contato.id}</td>
-            <td>${contato.name}</td>
-            <td>${contato.email}</td>
-        `
-        TABLE.tbody.appendChild(tr)
+    
+    obterContato(id) {
+        return this.contatos.find(contato => contato.id == id)
     }
 }
 
-const APP = {
-    init(){
-        const contatos = CONTATO.contatos
-        lengthContatos=contatos.length
-        if (lengthContatos>0){
-            TABLE.clearTable()
-            TABLE.navigateContacts()
+const funcHTML = {
+    nome: document.querySelector('input#name'),
+    email: document.querySelector('input#email'),
+    table: document.querySelector('table.pure-table tbody'),
+
+    limparTabela() {
+        this.table.innerHTML=''
+    },
+
+    adicionarElementos(){
+        const contatos = funcStorage.get()
+
+        // se tiver algum contato
+        if (contatos.length > 0) {
+            //apagar todas as informações
+            this.limparTabela()
+            //preencher a tabela
+            contatos.forEach(contato => {
+                const tr = document.createElement('tr')
+                tr.innerHTML=`
+                    <td>${contato.id}</td>
+                    <td>${contato.nome}</td>
+                    <td>${contato.email}</td>
+                    <td><a href="#">Editar</a> // <a href="#" onclick="funcContato.excluirContato(${contato.id})">Excluir</a></td>
+                `
+                this.table.appendChild(tr)
+            });
         }
     }
 }
 
-APP.init()
+function teste(){
+    funcContato.adicionarContato()
+}
+
+function iniciar(){
+    funcHTML.adicionarElementos()
+}
+
+iniciar()
